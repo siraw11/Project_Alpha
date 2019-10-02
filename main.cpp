c8ccdad13889e607c84a0880145b719214dd01d7
matteo <matteo.tinacci@stud.unifi.it>
2019-10-02 11:17:44 +0200

    room 1

--- main.cpp
+++ main.cpp
@@ -23,12 +23,14 @@ int main() {
         sf::RenderWindow window(sf::VideoMode::getDesktopMode(),"Project_Alpha");
         //set hero
         Hero hero(3,5,1,3);
-        hero.setPosition(32*10,32*10);
         auto heroTexture = new sf::Texture;
-        heroTexture->loadFromFile("Sprites/luca.png");
-        hero.setTexture(*heroTexture);
-        hero.setScale(sf::Vector2f(1.f*SCALE,1.f*SCALE));
-        hero.setTextureRect(sf::IntRect(32,0,32,32));
+    heroTexture->loadFromFile("Sprites/luca.png");
+    hero.setTexture(*heroTexture);
+
+    hero.setTextureRect(sf::IntRect(32,0,32,32));
+    hero.setOrigin(hero.getPosition().x + hero.getGlobalBounds().width/2,hero.getPosition().y + hero.getGlobalBounds().height/2);
+    hero.setPosition(32*10,32*10);
+    hero.setScale(sf::Vector2f(1.f*SCALE,1.f*SCALE));
         int counterWalking=0;
 
 
@@ -53,13 +55,14 @@ int main() {
                     3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
                     3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
                     3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
+                    3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
                     5, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 6,
             };
 
 
     // create the tilemap from the level definition
     Map map;
-    if (!map.load("Sprites/Tilemap2.png", sf::Vector2u(32, 32), level, 40, 20))
+    if (!map.load("Sprites/Tilemap2.png", sf::Vector2u(32, 32), level, 40, 21))
         return -1;
     map.setScale(sf::Vector2f(1.f*SCALE,1.f*SCALE));
 
@@ -81,29 +84,47 @@ int main() {
 
             window.clear(sf::Color::Black);
 
-            int x = (int)(hero.getPosition().x)/32/SCALE;
-            int y = (int)(hero.getPosition().y-1)/32/SCALE;
+
             if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
             {
-               if(level[40*x+y]==0){
-                    std::cout << level[40*x+y] << std::endl;
-                    hero.move(0,-hero.getSpeed());
-                }
+                int x = (int)(hero.getPosition().x)/32/SCALE;
+                int y = (int)(hero.getPosition().y-1)/32/SCALE;
+
+               if(level[40*y+x]==0){
+                   std::cout<<x<<" "<<y<<std::endl;
+                   hero.move(0,-hero.getSpeed());
+               }
+
+
                 hero.setTextureRect(sf::IntRect(32*counterWalking,32*3,32,32));
 
             }
 
             if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
             {
-                hero.move(0,hero.getSpeed());
+                int x = (int)(hero.getPosition().x)/32/SCALE;
+                int y = (int)(hero.getPosition().y+1)/32/SCALE;
+                if(level[40*y+x]==0){
+
+                    std::cout<<x<<" "<<y<<std::endl;
+                    hero.move(0,hero.getSpeed());
+
+                    }
 
                 hero.setTextureRect(sf::IntRect(32*counterWalking,0,32,32));
 
             }
 
             if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+
             {
-                hero.move(-hero.getSpeed(),0);
+                int x = (int)(hero.getPosition().x-1)/32/SCALE;
+                int y = (int)(hero.getPosition().y)/32/SCALE;
+                if(level[40*y+x]==0){
+                    std::cout<<x<<" "<<y<<std::endl;
+                    hero.move(-hero.getSpeed(),0);
+                }
+
 
                 hero.setTextureRect(sf::IntRect(32*counterWalking,32,32,32));
 
@@ -111,9 +132,12 @@ int main() {
             if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
 
             {
-                //if(( )
-                {hero.move(hero.getSpeed(),0);
-                hero.setTextureRect(sf::IntRect(32*counterWalking,32*2,32,32));}
+                int x = (int)(hero.getPosition().x+1)/32/SCALE;
+                int y = (int)(hero.getPosition().y)/32/SCALE;
+                if(level[40*y+x]==0){
+                    std::cout<<x<<" "<<y<<std::endl;
+                    hero.move(hero.getSpeed(),0);}
+                hero.setTextureRect(sf::IntRect(32*counterWalking,32*2,32,32));
             }
 
 
