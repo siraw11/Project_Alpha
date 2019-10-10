db3a9aa00f31e5c406bb5c705f55a55c6ea8044e
lucagraziotti <luca.g@protonmail.ch>
2019-10-10 09:13:11 +0200

    Addedd attribute "texture"

--- Item.cpp
+++ Item.cpp
@@ -8,7 +8,10 @@
 #include <SFML/Graphics.hpp>
 #include "Box2D/Box2D.h"
 
-Item::Item(double _posX, double _posY, double _width, double _height):posX(_posX),posY(_posY),width(_width),height(_height) {}
+Item::Item(double _posX, double _posY, double _width, double _height, std::string _texture) : posX(_posX), posY(_posY),
+                                                                                              width(_width),
+                                                                                              height(_height),
+                                                                                              texture(_texture) {}
 
 
 
@@ -48,4 +51,12 @@ Item::~Item() {}
 
 void Item::doSpecial() {
     std::cout << "Special!" << std::endl;
+}
+
+const std::string &Item::getTexture() const {
+    return texture;
+}
+
+void Item::setTexture(const std::string &texture) {
+    Item::texture = texture;
 }
