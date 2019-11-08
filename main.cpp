f463c599455020709739645184938f42af565a24
lucagraziotti <luca.g@protonmail.ch>
2019-11-08 18:32:16 +0100

    Implemented State Pattern

--- main.cpp
+++ main.cpp
@@ -1,18 +1,9 @@
-#include "SFML/Graphics.hpp"
-#include <iostream>
-#include "GameEngine.h"
-#include <vector>
+#include "GameStates/StateMachine.h"
+#include "Game.h"
 
-#include "Menu.h"
 
 int main() {
-
-    GameEngine *engine = new GameEngine();
-    sf::RenderWindow window(engine->video, "Motocross 2D", sf::Style::Default);//Create window withe default resolution
-    engine->window = &window;
-    window.setFramerateLimit(engine->framerate);
-
-    Menu menu(MenuType::Home, MenuOption::loadMainMenuOptions(), engine);
-    menu.open();
+    StateMachine stateMachine;
+    Game();
 
 }
