0eaf6e24d1378318cff491e2282567139c016a3b
lucagraziotti <luca.g@protonmail.ch>
2019-11-12 08:17:00 +0100

    Created GameLostState

---
+++ GameStates/GameLostState.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Luca Graziotti on 11/11/2019.
+//
+
+#include "GameLostState.h"
+
+void GameLostState::handleInput(sf::Event) {
+
+}
+
+void GameLostState::draw() {
+
+}
+
+void GameLostState::update() {
+
+}
