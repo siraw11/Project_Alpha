2e0f6f88503f7c26d5e5ce17a6a8fb32b1ea3637
lucagraziotti <luca.g@protonmail.ch>
2019-11-12 08:17:49 +0100

    Added end match checkpoint support

--- Checkpoint.cpp
+++ Checkpoint.cpp
@@ -2,7 +2,24 @@
 // Created by Magna on 2019-06-07.
 //
 
+#include <GameStates/GameWinState.h>
+#include "GameStates/StateMachine.h"
 #include "Checkpoint.h"
-void Checkpoint::doSpecial() {
+#include "Game.h"
 
+Checkpoint::Checkpoint(double _posX, double _posY, double _width, double _height, bool _isEnd, std::string _texture)
+        : Item(_posX,
+               _posY,
+               _width,
+               _height,
+               _texture), isEnd(_isEnd) {}
+
+
+void Checkpoint::doSpecial() {
+    if (isEnd) {
+        Game::gameData->machine.push_state(StateRef(new GameWinState()));
+    } else {
+        //TODO:da implementare salvataggio ultima posizione
+        std::cout << "checkpoint Save";
+    }
 }
