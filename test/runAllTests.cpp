2a1b9f3cc60eb9444b1307756f9f2b9681c279d2
lucagraziotti <luca.g@protonmail.ch>
2019-11-21 14:46:24 +0100

    Created main function

--- test/runAllTests.cpp
+++ test/runAllTests.cpp
@@ -1,4 +1,9 @@
 //
 // Created by Magna on 2019-06-03.
 //
+#include "gtest/gtest.h"
 
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
