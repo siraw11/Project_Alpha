5f16140d7fb2424ba6d62aafb6ccdbb273b46c07
lucagraziotti <luca.g@protonmail.ch>
2019-11-21 14:47:06 +0100

    Created tests

--- test/BikeFixture.cpp
+++ test/BikeFixture.cpp
@@ -2,9 +2,21 @@
 // Created by Magna on 2019-06-03.
 //
 
+#include <Game.h>
 #include "gtest/gtest.h"
-#include "../Bike.h"
+#include "Bike.h"
 
 class BikeSuite: public::testing::Test{
+protected:
+    Bike b;
 
+    virtual void SetUp() {
+        b.setIsUnlocked(true);
+        b.setId("b1");
+        b.setName("Bike");
+        b.setTexture("");
+        b.setPrice(300);
+        b.setSpeed(100);
+    }
 };
+
--- test/PlayerFixture.cpp
+++ test/PlayerFixture.cpp
@@ -4,6 +4,18 @@
 #include "gtest/gtest.h"
 #include "../Player.h"
 
-class PlayerSuite: public::testing::Test{
+class PlayerSuite : public ::testing::Test {
+protected:
+    Player p;
 
+    virtual void SetUp() {
+        p.setUsername("Player");
+        p.addTotalCoin(500);
+    }
 };
+
+TEST_F(PlayerSuite, TestCoin) {
+    p.removeTotalCoin(100);
+    p.addTotalCoin((100));
+    ASSERT_EQ(p.getTotalCoin(), 500);
+}
