a07f072db7a3b439348f2ff812676d7a142680f4
lucagraziotti <luca.g@protonmail.ch>
2019-11-24 16:22:50 +0100

    Removed percentage and observer

--- Match.cpp
+++ Match.cpp
@@ -85,7 +85,7 @@ const std::shared_ptr<Timer> &Match::getTimer() const {
 void Match::setTimer(const std::shared_ptr<Timer> &timer) {
     Match::timer = timer;
 }
-
+/*
 float Match::getPercentage() const {
     return percentage;
 }
@@ -93,18 +93,4 @@ float Match::getPercentage() const {
 void Match::setPercentage(float percentage) {
     Match::percentage = percentage;
 }
-
-
-void Match::registerObserver(Observer *o) {
-    observers.push_back(o);
-}
-
-void Match::removeObserver(Observer *o) {
-    observers.remove(o);
-}
-
-void Match::notifyObservers() {
-    for (auto itr = std::begin(observers); itr != std::end(observers); itr++) {
-        (*itr)->update();
-    }
-}
+*/
