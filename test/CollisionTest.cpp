1e26db68837e1eb95f5c9ee46546f799b346ff1b
waris <waris.amjed@stud.unifi.it>
2021-03-04 14:56:03 +0100

    gtest

--- test/CollisionTest.cpp
+++ test/CollisionTest.cpp
@@ -8,19 +8,12 @@
 #include "../Archer.h"
 
 
-TEST(test_suite_name,prova){
-    int a =2;
-    int b=2;
-    ASSERT_EQ(a,b);
-}
-
 TEST(Hero, wallCollision){
-    Hero *hero = new Archer(1,1,30,1,1,1);
+    Hero *hero = new Archer (1,1,30,1,1,1);
     Tile tile(0);
     sf::Vector2f position(412, 300);
     tile.setPosition(position);
     std::vector<Tile> tile_vector;
     tile_vector.push_back(tile);
     ASSERT_FALSE(Collision::checkCollision(tile_vector, hero, 1, 0));
-
 }
